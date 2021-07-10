b292e47506e8aa85e3c0afc3cc10d55e63aa23e0
hchen2007 <hungyuofficial@outlook.com>
2021-07-10 18:28:20 +0700

    everything la

--- lecture_3/lecture_3/hello/urls.py
+++ lecture_3/lecture_3/hello/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns ={
     path("", views.index, name="index"),
-    path("chen", views.chen, name="chen")
+    path("chen", views.chen, name="chen"),
+    path("steve", views.steve, name="steve"),
+    path("<str:name>", views.greet, name="greet")
 }
--- lecture_3/lecture_3/hello/views.py
+++ lecture_3/lecture_3/hello/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request,"hello/index.html")
 
 def chen(request):
     return HttpResponse("Hey there Chen!")
+
+def steve(request):
+    return HttpResponse("Hey there Steve!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
